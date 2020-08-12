2cae303fc6cb9afb34e8ea70898ca3ea63ff749b
franco giovannangelo <fcgiova@gmail.com>
2020-08-12 04:53:26 -0300

    env var cors to add our front-end url, optimization in composer.json, force https in AppServiceProvider, delete user default route from api routes

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/items', 'ItemController@availables');
 Route::post('/order', 'OrderController@api_store');
